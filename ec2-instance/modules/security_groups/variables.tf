d37fbe32fc3bf20ef335f0a48a3eec37c1bbcba9
Kjeff24 <jeffreyarthur123@gmail.com>
2025-05-10 08:45:00 +0000

    fix(security-group): simplified security group creation

--- ec2-instance/modules/security_groups/variables.tf
+++ ec2-instance/modules/security_groups/variables.tf
@@ -1,19 +1,3 @@
 variable "project_name" {
 	type = string
 }
-
-variable "vpc_id" {
-	type = string
-}
-
-variable "vpc_cidr" {
-	type = string
-}
-
-variable "ssh_allowed_cidrs" {
-	type = list(string)
-}
-
-variable "http_allowed_cidrs" {
-	type = list(string)
-}
