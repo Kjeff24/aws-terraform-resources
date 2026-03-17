cd95bc18293491f9b9d36ad547e49622985bc6d7
Kjeff24 <jeffreyarthur123@gmail.com>
2026-03-17 10:37:00 +0000

    chore: add backend config for aws-athena

---
+++ aws-athena/backend.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket       = "account-vending-terraform-state"
+    key          = "learn-guide/aws-athena.tfstate"
+    region       = "eu-west-1"
+    use_lockfile = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = var.tags
+  }
+}
