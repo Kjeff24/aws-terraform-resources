eb3814541dbeb2e519c8f35e2e8ff5973b159b11
Kjeff24 <jeffreyarthur123@gmail.com>
2026-03-21 16:15:00 +0000

    chore: add user_pool_domain output

--- aws-cognito/outputs.tf
+++ aws-cognito/outputs.tf
@@ -12,3 +12,8 @@ output "user_pool_client_id" {
   description = "The ID of the Cognito User Pool Client"
   value       = aws_cognito_user_pool_client.user_pool_client.id
 }
+
+output "user_pool_domain" {
+  description = "The Cognito hosted UI domain"
+  value       = aws_cognito_user_pool_domain.user_pool_domain.domain
+}
