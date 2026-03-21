5a10c8f000c12bd3d21fda477f639d783b80458e
Kjeff24 <jeffreyarthur123@gmail.com>
2026-03-21 16:12:00 +0000

    chore: add user_pool_client_id output

--- aws-cognito/outputs.tf
+++ aws-cognito/outputs.tf
@@ -7,3 +7,8 @@ output "user_pool_arn" {
   description = "The ARN of the Cognito User Pool"
   value       = aws_cognito_user_pool.user_pool.arn
 }
+
+output "user_pool_client_id" {
+  description = "The ID of the Cognito User Pool Client"
+  value       = aws_cognito_user_pool_client.user_pool_client.id
+}
