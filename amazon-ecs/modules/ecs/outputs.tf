ddba97ab354a33de3cb8ef84394a163710c05314
Kjeff24 <jeffreyarthur123@gmail.com>
2026-03-23 11:41:00 +0000

    feat(ecs): added ecs service outputs in outputs.tf

--- amazon-ecs/modules/ecs/outputs.tf
+++ amazon-ecs/modules/ecs/outputs.tf
@@ -13,3 +13,19 @@ output "cluster_name" {
   description = "ECS cluster name"
   value       = aws_ecs_cluster.app_cluster.name
 }
+
+# ECS Service Outputs
+output "service_id" {
+  description = "ECS service ID"
+  value       = aws_ecs_service.client_service_management.id
+}
+
+output "service_name" {
+  description = "ECS service name"
+  value       = aws_ecs_service.client_service_management.name
+}
+
+output "service_arn" {
+  description = "ECS service ARN"
+  value       = aws_ecs_service.client_service_management.id
+}
