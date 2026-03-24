ad1bc56058fd415c695ae7831881295579864cc2
Kjeff24 <jeffreyarthur123@gmail.com>
2026-03-24 18:34:00 +0000

    feat(iam): ecs task execution role

---
+++ amazon-ecs/modules/iam/main.tf
@@ -0,0 +1,19 @@
+
+
+# ===============================
+# ECS Task Execution Role
+# Used by ECS to pull images, fetch secrets, and send logs
+# ===============================
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.project_name}-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "ecs-tasks.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+
+}
