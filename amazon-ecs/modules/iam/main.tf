ea69ceb510e3f2bc43a654ee6050cb42a62e1f5d
Kjeff24 <jeffreyarthur123@gmail.com>
2026-03-24 18:51:00 +0000

    feat(iam): added ecs task role

--- amazon-ecs/modules/iam/main.tf
+++ amazon-ecs/modules/iam/main.tf
@@ -23,3 +23,20 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_managed" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+# ===============================
+# ECS Task Role (App inside container)
+# ===============================
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${var.project_name}-ecs-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "ecs-tasks.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+
+}
