b329e001241c5d7a455b49621a3189e656c12584
Kjeff24 <jeffreyarthur123@gmail.com>
2026-03-26 17:47:00 +0000

    chore: add alb module to main

--- amazon-ecs/main.tf
+++ amazon-ecs/main.tf
@@ -9,3 +9,14 @@ module "vpc" {
   private_subnet_cidrs = var.private_subnet_cidrs
   container_port       = var.ecs_config.container_port
 }
+
+module "alb" {
+  source = "./modules/alb"
+
+  project_name          = var.project_name
+  vpc_id                = module.vpc.vpc_id
+  public_subnet_ids     = module.vpc.public_subnet_ids
+  alb_security_group_id = module.vpc.alb_security_group_id
+  container_port        = var.ecs_config.container_port
+  health_check_path     = var.health_check_path
+}
