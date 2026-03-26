d626f03e82a53573fda58ece10a4b229c9fac761
Kjeff24 <jeffreyarthur123@gmail.com>
2026-03-26 18:14:00 +0000

    chore: add ecs module to main

--- amazon-ecs/main.tf
+++ amazon-ecs/main.tf
@@ -27,3 +27,18 @@ module "iam" {
   region       = var.region
   project_name = var.project_name
 }
+
+module "ecs" {
+  source = "./modules/ecs"
+
+  region             = var.region
+  project_name       = var.project_name
+  private_subnet_ids = module.vpc.private_subnet_ids
+  security_group_id  = module.vpc.ecs_security_group_id
+  target_group_arn   = module.alb.target_group_arn
+  execution_role_arn = module.iam.task_execution_role_arn
+  task_role_arn      = module.iam.task_role_arn
+  health_check_path  = var.health_check_path
+  db_password        = var.db_password
+  ecs_config         = var.ecs_config
+}
