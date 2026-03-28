168922cefa997646a9ce84e983de3159a71af12a
Kjeff24 <jeffreyarthur123@gmail.com>
2026-03-28 16:32:00 +0000

    chore: add lambda module variables

---
+++ amazon-api-gateway-lambda/modules/lambda/variables.tf
@@ -0,0 +1,36 @@
+############################
+# 🌍 General Configuration
+############################
+variable "region" {
+  description = "AWS region for deployment"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name used for resource naming and tagging"
+  type        = string
+}
+
+############################
+# 🔐 IAM
+############################
+variable "execution_role_arn" {
+  description = "ARN of the Lambda execution role"
+  type        = string
+}
+
+############################
+# ⚡ Lambda Configuration
+############################
+variable "lambda_config" {
+  description = "Configuration for the Lambda function"
+  type = object({
+    runtime       = string
+    handler       = string
+    filename      = string
+    timeout       = number
+    memory_size   = number
+    architectures = list(string)
+    environment_variables = map(string)
+  })
+}
