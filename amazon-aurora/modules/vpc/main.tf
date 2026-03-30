87b18de937b6052edee880cffa619421bbf55047
Kjeff24 <jeffreyarthur123@gmail.com>
2026-03-30 21:19:00 +0000

    chore: add private subnet resource

--- amazon-aurora/modules/vpc/main.tf
+++ amazon-aurora/modules/vpc/main.tf
@@ -34,3 +34,16 @@ resource "aws_vpc" "main" {
     ResourceName = "VPC"
   }
 }
+
+# 🔒 Private Subnets
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name         = "${var.project_name}-private-subnet-${count.index + 1}"
+    ResourceName = "PrivateSubnet"
+  }
+}
